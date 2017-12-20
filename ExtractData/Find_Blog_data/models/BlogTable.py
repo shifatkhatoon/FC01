12b1de5431927f0b6edafda2d2dd5d374e71c50f
arshul <arshulmohd123@gmail.com>
2017-12-20 12:41:42 -0500

    migrations deleted code set up

--- ExtractData/Find_Blog_data/models/BlogTable.py
+++ ExtractData/Find_Blog_data/models/BlogTable.py
@@ -1,14 +1,16 @@
 from Find_Blog_data import db
 from Find_Blog_data.models.base import Base
 
+
+
+    
 class Blogs(Base):
     blog_url = db.Column(db.String(100))
     blog_title = db.Column(db.String(200))
-    blog_imageUrl = db.relationship('Image', backref='Blogs', lazy=True)
     blog_Author_Name = db.Column(db.String(50))
     blog_Posted_Date = db.Column(db.String(10))
     blog_Content = db.Column(db.String(length=None))
-
+    blog_imageUrl = db.relationship('Image', backref='Blogs', lazy=True)
     def __init__(self, blog):
         self.blog_url = blog['url']
         self.blog_title = blog['title']
@@ -20,12 +22,12 @@ class Blogs(Base):
         return '<%s%s%s%s%s%s>' %(self.blog_title,self.blog_url,self.blog_imageUrl,self.blog_Author_Name,
                                   self.blog_Posted_Date, self.blog_Content)
 
-class Image(db.Model):
-    Blogs_id = db.Column(db.Integer, db.ForeignKey('Blogs.id'), nullable=False)
-    blog_ImageUrl = db.Column(db.String(500))
 
+
+class Image(Base):
+    Blogs_id = db.Column(db.Integer, db.ForeignKey('blogs.id'), nullable=False)
+    blog_ImageUrl = db.Column(db.String(500))
+    
     def __init__(self,url,blog_id):
         self.Blogs_id = blog_id
         self.blog_ImageUrl = url
-    
-
